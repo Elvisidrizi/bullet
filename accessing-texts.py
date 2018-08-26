2535eee7d6b61cf0a4408c513d4f0351e0ddbd2e
Elvis <32521623+Elvisidrizi@users.noreply.github.com>
2018-08-26 05:43:46 +0200

    Add files via upload

---
+++ accessing-texts.py
@@ -0,0 +1,15 @@
+from nltk.book import *
+
+#Access texts by calling out their names
+#print(texts())
+
+print('\n\n\n')
+# texts() function
+print(sent1)
+
+# sents() function
+#print(sents(), end='\n\n\n')
+
+# access sentences individually
+#print(sent1)
+
